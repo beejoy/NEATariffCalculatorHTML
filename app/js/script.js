ad6577867290ca3977cc0b2d4736ec00c07c4408
Bijay Rai <bijay.chamling@gmail.com>
2020-07-26 15:55:37 +0545

    Updated UI

--- app/js/script.js
+++ app/js/script.js
@@ -1,12 +1,18 @@
-const btnCalculate = document.getElementById("btnCalculate");
-const currentReading = document.getElementById("currentReading");
-const previousReading = document.getElementById("previousReading");
+const btnCalculate = document.querySelector("#btnCalculate");
+
 const totalUnits = document.getElementById("totalUnits");
 const ampereDemand = document.getElementById("ampereDemand");
 const totalTariff = document.getElementById("totalTariff");
 
 btnCalculate.addEventListener('click', function() {
     console.log('btnCalculate_click()');
+
+    var currentReading = parseInt(document.getElementById("currentReading").value);
+    var previousReading = parseInt(document.getElementById("previousReading").value);
+
+    totalUnits.value = currentReading - previousReading;
+    totalTariff.value = ampereDemand.value;
+
+    return false;
     
-    totalUnits.innerHTML = calc(currentReading - previousReading);
 });
